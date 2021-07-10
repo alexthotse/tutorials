360fe30eecb6a090b3f9800d4f900674c4dba047
Alex Thotse <30655517+alexthotse@users.noreply.github.com>
2021-07-10 14:42:07 +0200

    Go Bootcamp

---
+++ Go/Go_Bootcamp/Chapter2_The_Basics/2.2.constants.go
@@ -0,0 +1,10 @@
+/*
+2.2 Constants
+2.3 Printing Constants and Variables
+2.4 Packages and imports
+2.5 Code location
+2.6 Exported names
+2.7 Functions, signature, return values, named results
+2.8 Pointers
+2.9 Mutability
+*/ 
